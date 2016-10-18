c84e3328886a6a634af4b6a632ee3f7eb5c5b2b9
Kathy Toufighi <kathy.toufighi@gmail.com>
2016-10-18 15:16:53 -0400

    working with iterators in rruby, the Bitmaker ex.

---
+++ exercise4.rb
@@ -0,0 +1,11 @@
+100.times do |number|
+  if (number % 3 == 0) && (number % 5 == 0)
+    puts "BitMaker"
+  elsif number % 5 == 0
+    puts "Maker"
+  elsif number % 3 == 0
+    puts "Bit"
+  else
+    puts number
+  end
+end
