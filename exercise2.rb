9478b69b20515c89d52433d44da44aaa0af6fa59
Kathy Toufighi <kathy.toufighi@gmail.com>
2016-10-18 14:28:00 -0400

    added more lines strings and integers and number of flowers in my garden

--- exercise2.rb
+++ exercise2.rb
@@ -2,3 +2,17 @@
 
 tips = 55.to_f * 2/10.to_f
 puts "a good amount of tip for 55 dollars would be #{tips}"
+
+
+# Find a way to convert the integer into a string
+
+puts "my name is Kathy Toufighi and I am " + 41.to_s + " years old."
+
+
+# output two integer values multiplied in a sentence structure.
+
+puts "I have #{45628 * 7839} flowers in my garden."
+
+#what is the value (10 < 20 && 30 < 20) || !(10 == 11)
+
+true
